2f572a45be31e3e35934a9fe90c15ed6d2f7ca38
Bernhard Nebel <nebel@hinterm-ziel.de>
2026-05-03 14:35:45 +0200

    tests

--- tests/end-to-end/sketches/cmin/cmin.c
+++ tests/end-to-end/sketches/cmin/cmin.c
@@ -1,5 +1,13 @@
 #include <avr/io.h>
-
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+#pragma arduino build_flags -wall -DUSE_BUFFER
+#pragma arduino
+/* 
+#pragma arduino linkflags 
+*/
+#pragma GCC diagnostic pop
+#pragma Blabla
 int main()
 {
   return 0;
